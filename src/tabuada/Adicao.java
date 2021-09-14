d0192c40fc2d6237ce1510fe30562f8daf722735
Ariana Camargos <86811983+AriCamargos@users.noreply.github.com>
2021-09-14 12:29:42 -0300

    tabuada for

---
+++ src/tabuada/Adicao.java
@@ -0,0 +1,17 @@
+package tabuada;
+/**
+*@author AriCamargos
+*@version 8.0 (Sept 13, 2021)
+* estudando estutura de repetição FOR
+**/
+public class Adicao {
+    /*public static void main(String[] args) {
+        int numero = 10;
+        System.out.println(" == Tabuada de adição do nº" + numero + " ==");
+        for(int i=1; i<=10; i++){
+            int resultado = numero + i;
+            System.out.println(numero + " + " + i + " = " + resultado);
+            
+        }
+    }*/
+}
---
+++ src/tabuada/Subtracao.java
@@ -0,0 +1,16 @@
+package tabuada;
+/**
+*@author AriCamargos
+*@version 8.0 (Sept 13, 2021)
+* estudando estutura de repetição FOR
+**/
+public class Subtracao {
+    /*public static void main(String[] args){
+        int numero = 7;
+        System.out.println("== Tabuada de subtração nº" + numero + " ==");
+        for(int i=1; i <= 7; i++){
+            int resultado = numero - i;
+            System.out.println(numero + " - " + i + " = " + resultado);
+        }
+    }*/
+}
